f1f65d85246889c4b978164f4c31725559ffc376
krupali <vekariyakrupali506@gmail.com>
2021-04-23 11:11:49 -0400

    [Fix]change of comments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { FormsModule  } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { Exercise1Component } from './exercise1/exercise1.component';
+import { HighlightDirective } from './highlight.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Exercise1Component
+    Exercise1Component,
+    HighlightDirective,
   ],
   imports: [
     BrowserModule,
--- src/app/exercise1/exercise1.component.ts
+++ src/app/exercise1/exercise1.component.ts
@@ -8,84 +8,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Exercise1Component implements OnInit {
  
-  //For Exercise 1:
+  //Find length of string variable....
   txtString:string="";
   btnEnable=false;
   txtStringLength=0;
 
 
-  //For Exercise 2:
+  //Display name and age object....
   person={
     txtName:"",
     txtAge:""};
-    txtDisplay="";
-    showBtn1=true;
-    showBtn2=false;
+    txtDisplay=[];
+    btnHide=false;
 
-  //For Exercise 3:
+  //Display name and age array....
   array=[];
   txtStringName;
   txtStringAge;
   txtDisplayArray="";
 
-  //For Exercise 4:
+  //Display odd or even number variable....
   txtNumber;
-  txtDisplayNumber="";
-  numberStatus="odd";
+  txtEven='even nuumber';
+  txtOdd='odd number';
 
   constructor() { }
 
   ngOnInit() {
   }
-
- 
-
-  onBtnClick(){
-    this.btnEnable=true;
-  }
+//Find a length of input string...
   onTxtStringLength(){
     this.txtStringLength = this.txtString.length;
     this.btnEnable=false;
     this.txtString=null;
   }
-  //for Exercise 2:
+  //Display array of object...
 
   onTxtDisplay(){
-    this.txtDisplay  = this.person.txtName  + " " +this.person.txtAge ;
-    this.showBtn1 = false;
-    this.showBtn2 = true;
+    this.txtDisplay.push([this.person.txtName,this.person.txtAge]);
+    this.btnHide=true;
+    this.person.txtName=null;
+    this.person.txtAge=null;
   }
-
-  onBtnHidden(){
-      this.showBtn1 = true;
-      this.showBtn2 = false;
-      this.txtDisplay="";
-  }
-
-  //For Exercise 3:
+  //Display array of value....
   onArrayDisplay() {
     this.array.push([this.txtStringName,this.txtStringAge]);
+    this.txtStringName=null;
+    this.txtStringAge=null;
   }
-
-  //For Exercise 4:
-  
-  onDisplayOddEven(){
-
-    if( this.txtNumber % 2==0){
-      this.numberStatus='even';
-      this.txtDisplayNumber=this.txtNumber +' is Even Numbar';
-    }
-    else{
-      this.numberStatus='odd';
-      this.txtDisplayNumber=this.txtNumber +' is Odd Number';
-    } 
-  }
-  getColor(){
-    return this.numberStatus ==='odd' ? 'red' : 'green';
-  }
-   spanColor='false';
-  getBackColor(){
-    this.spanColor='true';
-   return this.spanColor === 'true' ? 'yellow' : 'white';
-  } 
 }
